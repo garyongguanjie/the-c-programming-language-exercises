aa8beac13c09948763b078ddf6660d08262fed26
Gary Ong <garyong.ahs@gmail.com>
2024-04-27 01:37:37 +0800

    added bit manipulation exercises

--- exercise-2/exercise2-6-8.c
+++ exercise-2/exercise2-6-8.c
@@ -3,6 +3,7 @@
 unsigned getbits(unsigned x, int p, int n);
 unsigned setbits(unsigned x, int p, int n, int y);
 unsigned invert(unsigned x, int p, int n);
+unsigned rightrot(unsigned x, int n);
 
 int main()
 {
@@ -24,12 +25,20 @@ int main()
 
 
     printf("Testing invert bits\n");
-    x = 0b1000000;
-    p  = 0;
-    n = 1;
+    x = 0b00101101101;
+    p  = 6;
+    n = 4;
     result = invert(x,p,n);
     printf("Result: 0x%X\n", result);
+
+
+    printf("Testing right rot\n");
+    x = 0b100001101;
+    n = 4;
+    result = rightrot(x,n);
+    printf("Result: 0x%X\n", result);
     return 0;
+
 }
 
 /* getbits: get n bits from position p */
@@ -49,5 +58,15 @@ unsigned setbits(unsigned x, int p, int n, int y)
 
 // returns x with the n bits that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.
 unsigned invert(unsigned x, int p, int n){
-    
+    unsigned zero =  0;
+    int mask1 = ~zero << (p+1-n);
+    int mask2 = ~zero >> (32-p-1);
+    // using xor here to invert the bits is kind of hard to figure out?
+    return x ^ (mask1 & mask2);
+}
+
+// Write a function rightrot(x,n) that returns the value of the integer x rotated to the right by n positions
+unsigned rightrot(unsigned x, int n){
+    unsigned left = x << (32-n);
+    return (x >> n) | left;
 }
