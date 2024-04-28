8f673543102dfbbbc29c0877539bfab5c278d1a0
Gary Ong <garyong.ahs@gmail.com>
2024-04-28 20:08:39 +0800

    done with padding

---
+++ exercise-3/exercise3-6.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+void itoa(int n, char s[],int w);
+
+int main(){
+    char s[1000];
+    int n = 100;
+    int w;
+    itoa(n,s,10);
+    printf(s);
+    return 0;
+}
+
+/* itoa: convert n to characters in s */
+
+//In a two's complement number representation, our version of itoa does not handle the largest negative number,
+// that is, the value of n equal to -(2wordsize-1). 
+// Explain why not. Modify it to print that value correctly, regardless of the machine on which it runs.
+void itoa(int n, char s[],int w)
+{
+    int i;
+    int p;
+    unsigned un;
+
+    if (n < 0) /* record sign */
+        un = -n; /* make n positive */
+    else
+        un = n;
+
+    i = 0;
+    do
+    {                          /* generate digits in reverse order */
+        s[i++] = un % 10 + '0'; /* get next digit */
+    } while ((un /= 10) > 0);   /* delete it */
+    if (n < 0)
+        s[i++] = '-';
+
+    p = w-i-1;
+    while(p>=0){
+        s[i++]= ' ';
+        p--;
+    }
+    s[i] = '\0';
+    strrev(s);
+}
